d8ed5fc21bc4d4c4cdfc1bab69268b4b177593b2
Mallory Wittwer <mallory.wittwer@epfl.ch>
2025-04-18 22:32:36 +0200

    Add online edit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from fastapi import FastAPI, Form, HTTPException, Request
-from fastapi.responses import HTMLResponse, FileResponse
+from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel, HttpUrl, ValidationError
-from build import generate_pdf  # Import the generate_pdf function
-from fastapi.responses import StreamingResponse
+
+from build import generate_pdf
+
 
 app = FastAPI()
 
-# Middleware for CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -18,30 +18,55 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Mount static files
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Set up templates
 templates = Jinja2Templates(directory="templates")
 
 
-# Route to serve the form
 @app.get("/", response_class=HTMLResponse)
 async def read_form(request: Request):
     return templates.TemplateResponse("form.html", {"request": request})
 
 
-# Pydantic model for validation
 class FormData(BaseModel):
     author: str
     topic: str
     image_url: HttpUrl
+    facts: str
+    trust: str
+    missing: str
+    interpretations: str
+    difficult: str
+    assumptions: str
+    emotions: str
+    experiences: str
+    yourself: str
+    others: str
+    positive: str
+    negative: str
+    summary: str
 
 
-# Route to handle form submission
 @app.post("/submit")
 async def submit_form(
-    author: str = Form(...), topic: str = Form(...), image_url: str = Form(...)
+    author: str = Form(...),
+    topic: str = Form(...),
+    image_url: str = Form(
+        default="https://raw.githubusercontent.com/MalloryWittwer/opinion-development-notebook/refs/heads/main/assets/self_reflection.png"
+    ),
+    facts: str = Form(default=""),
+    trust: str = Form(default=""),
+    missing: str = Form(default=""),
+    interpretations: str = Form(default=""),
+    difficult: str = Form(default=""),
+    assumptions: str = Form(default=""),
+    emotions: str = Form(default=""),
+    experiences: str = Form(default=""),
+    yourself: str = Form(default=""),
+    others: str = Form(default=""),
+    positive: str = Form(default=""),
+    negative: str = Form(default=""),
+    summary: str = Form(default=""),
 ):
     if len(topic) > 100:
         raise HTTPException(
@@ -49,16 +74,69 @@ async def submit_form(
         )
 
     try:
-        form_data = FormData(author=author, topic=topic, image_url=image_url)
+        form_data = FormData(
+            author=author,
+            topic=topic,
+            image_url=image_url,
+            facts=facts,
+            trust=trust,
+            missing=missing,
+            interpretations=interpretations,
+            difficult=difficult,
+            assumptions=assumptions,
+            emotions=emotions,
+            experiences=experiences,
+            yourself=yourself,
+            others=others,
+            positive=positive,
+            negative=negative,
+            summary=summary,
+        )
     except ValidationError as e:
         raise HTTPException(status_code=400, detail="Invalid Image URL format.")
 
-    pdf_bytes = generate_pdf(form_data.topic, form_data.author, form_data.image_url, target="bytes")
+    pdf_bytes = generate_pdf(
+        form_data.topic,
+        form_data.author,
+        form_data.image_url,
+        form_data.facts,
+        form_data.trust,
+        form_data.missing,
+        form_data.interpretations,
+        form_data.difficult,
+        form_data.assumptions,
+        form_data.emotions,
+        form_data.experiences,
+        form_data.yourself,
+        form_data.others,
+        form_data.positive,
+        form_data.negative,
+        form_data.summary,
+        target="bytes",
+    )
 
     return StreamingResponse(
         pdf_bytes,
         media_type="application/pdf",
         headers={
             "Content-Disposition": f"attachment; filename={form_data.topic.lower().replace(' ', '-')}_notebook.pdf"
+        },
+    )
+
+
+@app.post("/edit", response_class=HTMLResponse)
+async def edit_notebook(
+    request: Request,
+    author: str = Form(...),
+    topic: str = Form(...),
+    image_url: str = Form(...),
+):
+    return templates.TemplateResponse(
+        "edit.html",
+        {
+            "request": request,
+            "author": author,
+            "topic": topic,
+            "image_url": image_url,
         },
     )
